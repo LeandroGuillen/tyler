de3e11dc0721bde494b88b267fc897493a363c4f
Leandro Guillen <leandroguillen@cgmail.com>
2015-06-30 11:10:46 +0200

    Adapted to new interface

--- src/tyle/protocol/TCPMessage.java
+++ src/tyle/protocol/TCPMessage.java
@@ -1,12 +1,17 @@
-package Tyle;
+package tyle.protocol;
 
+/**
+ * A TCPMessage will build messages from a TCP byte stream in the form:
+ * 
+ * 16 bytes ID + 8 bytes Temperature
+ */
 import java.io.DataInputStream;
 import java.io.IOException;
 
 public class TCPMessage extends Message {
 
 	public TCPMessage(DataInputStream data) throws IOException {
-		// TCP: 16 bytes for ID + 2 bytes measurement
+		
 		byte[] b = new byte[16];
 		
 		System.out.println("Reading " + data.read(b, 0, 16) + " bytes as ID");
@@ -14,6 +19,11 @@ public class TCPMessage extends Message {
 		
 		System.out.println("Reading 2 bytes as Measurement");
 		setMeasurement(data.readShort());
+	}
+
+	@Override
+	public String getMeasurementName() {
+		return "temperature";
 	}
 
 }
